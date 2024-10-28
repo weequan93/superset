4db0ae0dd6cb1b906bd657cb6ed97542154e16ec
weequan <wee@hyperlegend.onmicrosoft.com>
2024-10-28 17:25:23 +0800

    merge

---
+++ docker/pythonpath/superset_config_docker.py
@@ -0,0 +1,36 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# This file is included in the final Docker image and SHOULD be overridden when
+# deploying the image to prod. Settings configured here are intended for use in local
+# development environments. Also note that superset_config_docker.py is imported
+# as a final step as a means to override "defaults" configured here
+#
+import logging
+import os
+
+from celery.schedules import crontab
+from flask_caching.backends.filesystemcache import FileSystemCache
+
+logger = logging.getLogger()
+TIME_GRAIN_ADDONS = {'PT1S': '1 second'}
+
+TIME_GRAIN_ADDON_EXPRESSIONS = {
+    'clickhouse': {
+        'PT1S': 'toDateTime(intDiv(toUInt32(toDateTime({col})), 1)*1)'
+    }
+}
